740b7a734174e12ebea1d1f20d6e0e3cceed8743
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-04-24 21:59:58 +0530

    New Website UI using Astro  (#1)
    * Remove Jekyll setup and migrate to Astro framework
    
    * Update README.md
    
    * Remove unused dependencies from bun.lock file

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
+import icon from "astro-icon";
+import solidJs from "@astrojs/solid-js";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://muhammadfiaz.github.io",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://muhammadfiaz.github.io/sitemap-index.xml",
+        "https://muhammadfiaz.github.io/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+    icon(),
+    svelte(),
+  ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  build: {
+    prerender: true,
+    base: "/muhammadfiaz.github.io/",
+  },
+});
